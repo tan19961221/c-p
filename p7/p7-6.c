0d1b6ab48f073e3d99fd0d50149a133557d31d0a
tan19961221 <edz@tandongdong.local>
2020-02-13 12:28:58 +0800

    c

--- p7/p7-6.c
+++ p7/p7-6.c
@@ -1,14 +1,12 @@
 /*---编程确认对无符号整数执行算术运算不会发生数据溢出。---*/
 
 #include <stdio.h>
-
-
+#include <limits.h>
 int main(void)
 {
-    unsigned i, j;
-
-    printf("无符号整型的最大值为： %u\n", );
-
-    
-    return 0;
+	unsigned int x;
+	x = UINT_MAX;
+	printf("x = %u\n",x);
+	printf("x + 1 = %u\n",x+1);
+	return 0;
 }
