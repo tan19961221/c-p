214e314f8b51710c37699e686383e988b41eae9a
tan19961221 <edz@tandongdong.local>
2020-02-14 09:08:48 +0800

    c

--- p6/p6-13.c
+++ p6/p6-13.c
@@ -29,10 +29,10 @@ int main(void)
 {
 	int tensu[2][4][3] = { { { 91, 63, 78 },{ 67, 72, 46 },{ 89, 34, 53 },{ 32, 54, 34 } } ,
 						   { { 97, 67, 82 },{ 73, 43, 46 },{ 97, 56, 21 },{ 85, 46, 35 } } };
-	int sum[4][3];			/* 总分 */
-	mat_add(tensu[0], tensu[1], sum);				/* 求两次考试中成绩的总和 */
-	puts("第一次考试的分数");  mat_print(tensu[0]);	/* 显示第一次考试的分数 */
-	puts("第二次考试的分数");  mat_print(tensu[1]);	/* 显示第二次考试的分数 */
-	puts("总分");        mat_print(sum);		/* 显示总分 */
+	int sum[4][3];		
+	mat_add(tensu[0], tensu[1], sum);			
+	puts("第一次考试的分数");  mat_print(tensu[0]);	
+	puts("第二次考试的分数");  mat_print(tensu[1]);	
+	puts("总分");        mat_print(sum);		
 	return 0;
 }
